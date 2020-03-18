ddd0016e6fa4d1212b5d613cc8ed074fd400567f
somesh <someshkhandelia@gmail.com>
2020-03-18 08:35:48 +0530

    Working with templates

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+			{
+				'author'     : 'Somesh Khandelia',
+				'title' 	 : 'Blog Post 1',
+				'content'    : 'This is the first post content.',
+				'date_posted': 'March 15, 2020'
+			},
+			{
+				'author'     : 'Yukty Khandelia',
+				'title' 	 : 'Blog Post 2',
+				'content'    : 'This is the second post content.',
+				'date_posted': 'March 16, 2020'
+			}
+		]
+
+
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+				'posts': posts,
+				'title': 'Home'
+			  }
+	# The templates folder is automatically detected by django 
+	return render(request, 'blog/home.html', context)
 
 
 def about(request):
-	return render(request, 'blog/about.html')
+	context = {
+				'title': 'About'
+			  }
+	return render(request, 'blog/about.html', context)
